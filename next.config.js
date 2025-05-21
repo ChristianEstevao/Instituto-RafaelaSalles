f64b11c4bcb1c335e12e793ba6e0813501a40797
ChristianEstevao <96703754+ChristianEstevao@users.noreply.github.com>
2025-05-21 01:02:04 +0000

    Restored to '90d016a8664e301ba2c4f4a1619d9ed940939c3c'
    Replit-Restored-To: 90d016a8664e301ba2c4f4a1619d9ed940939c3c

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-
-const withNextIntl = require('next-intl/plugin')();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
